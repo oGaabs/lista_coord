2a693e5d12d4272ec57b54234c44d54248425d03
PuniGC <gabriel04.ok@gmail.com>
2022-12-01 16:21:37 -0300

    [+] first commit

---
+++ android/app/src/main/kotlin/com/example/listacoord/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.listacoord
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'listacoord.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "lista coordenadas",
+    home: listacoord(),
+  ));
+}
